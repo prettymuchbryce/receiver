6c9b2333c461af3bc27e62b5dbac149c105b4200
Bryce Neal <brycedneal@bryce-mbp.local>
2013-07-25 18:15:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+> A simple deployment tool that sits on your server and keeps your repositories up to date.
+
+
+![](http://i.imgur.com/3D1ou4b.jpg)
+
+# Receiver
+
+##Is this for me ?
+- You are using github to store your code
+- You have a server somewhere that hosts your project
+- You would like to push to github, and see the changes live on your server
+
+##Detailed instructions using forever
+
+1. ssh into your box
+
+2. `npm install -g forever`
+
+3. `forever start yourapp.js`
+
+4. git clone http://www.github.com/prettymuchbryce/receiver
+
+5. Edit receiver/config.json to specify the file system location of the git directories to pull.
+
+6. `forever start receiver/receiver.js`
+
+
+7. Supply this new app to github as a service hook. (Settings -> Service Hooks -> Webhook URLs)
+
+	example: http://www.mywebapp.com:3617/gitpush
+
+
+8. Push to github to deploy.
---
+++ receiver.js
@@ -0,0 +1,46 @@
+var config = require(__dirname + '/config.json');
+
+var express = require('express');
+var app = express();
+var port = config.port;
+var exec = require('child_process').exec;
+var command = constructCommand();
+
+app.post(config.endpoint, function(req, res) {
+  console.log("Receiving push.. Hut.. Hut... Hike!");
+  child = exec(command, function (error, stdout, stderr) {
+    console.log(stdout);
+  });
+  res.end("200 OK");
+});
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
+app.listen(port);
+console.log("Waiting for git push..");
+
+function constructCommand() {
+  var command = "";
+
+  for (var i = 0; i < config.directoriesToPull.length; i++) {
+    command += "cd " + config.directoriesToPull[i] + " && ";
+  }
+
+  command += "git pull && ";
+
+  for (var j = 0; j < config.commandsToRunAfterwards.length; j++) {
+    command += config.commandsToRunAfterwards[j];
+    if (j != config.commandsToRunAfterwards.length-1) {
+      command += " && ";
+    }
+  }
+  return command;
+}
